4da4f8a297c8367258747d5567fb163126785615
Pulkita Verma <pulkita.verma2023@ssipmt.com>
2026-02-08 12:40:46 +0530

    Update code

--- arr2.c
+++ arr2.c
@@ -1,21 +1,24 @@
-// 10,20,5,90,27,70,2
-// put it on the array
-// print the largest and smallest index
-#include<stdio.h>
-int main(){
-    int arr[7]={10,20,5,90,27,70,2};
-    // largest and smallest index initialize with the first element of the array
-    int largest = arr[0];
-    int smallest = arr[0];
-    for(int i=1;i<7;i++){
-        if(arr[i]>largest){
-            largest = arr[i];
+// // 10,20,5,90,27,70,2
+// // put it on the array
+// // print the largest and smallest index
+// // index of both largest and smallest
+#include <stdio.h>
+int main() {
+    int arr[7] = {10, 20, 5, 90, 27, 70, 2};
+    int maxIndex = 0;
+    int minIndex = 0;
+    for (int i = 1; i < 7; i++) {
+        if (arr[i] > arr[maxIndex]) {
+            maxIndex = i;
         }
-        if(arr[i]<smallest){
-            smallest = arr[i];
+        if (arr[i] < arr[minIndex]) {
+            minIndex = i;
         }
     }
-    printf("largest: %d\n",largest);
-    printf("smallest: %d\n",smallest);
+    printf("Largest value: %d\n", arr[maxIndex]);
+    printf("Largest index: %d\n", maxIndex);
+
+    printf("Smallest value: %d\n", arr[minIndex]);
+    printf("Smallest index: %d\n", minIndex);
     return 0;
 }
